1a5ef0f450f421801b6b2eaf6c46ad6ce3cc1835
CheHsuan <exas60807@gmail.com>
2015-10-13 12:12:41 +0800

    The fist version for q2
    *implement the main function for reading test file and searching smallest character
    *iterative.c is using for-loop to search smallest character
    *recursive.c is using recursive function to search smallest character

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,33 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
 
-int main()
+#define MAX_ARRAY_LENGTH 30
+
+char smallest_character(char str[], char c)
 {
+	for(int i = 0; i < strlen(str); i++){
+		if((str[i] - c) > 0)
+			return str[i];
+	}
+	return str[0];
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char *token1, *token2;
+	char *delim = " \t\n";
+	char buffer[MAX_ARRAY_LENGTH];
+
+	if((fp = fopen(argv[1], "r")) == NULL){
+		printf("can't not open the file\n");
+		return -1;
+	}
+
+	while(fgets(buffer, sizeof(buffer), fp) != NULL){
+		token1 = strtok(buffer,delim);
+		token2 = strtok(NULL, delim);
+		printf("%c\n",smallest_character(token1, *token2));
+	}
     return 0;
 }
